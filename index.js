f8cf772f686629928ca8c1d67a11b84ca9bc29f6
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2023-06-28 01:44:12 +0300

    8 done

---
+++ index.js
@@ -0,0 +1,93 @@
+/***** 1 ****/
+// function checkAge(age) {
+//   if (age >= 18) {
+//     // Change this line
+//     return 'You are an adult';
+//   }
+//   return 'You are a minor';
+// }
+// let message = checkAge();
+// message = checkAge(20);
+// console.log(message);
+// message = checkAge(8);
+// console.log(message);
+// message = checkAge(14);
+// console.log(message);
+// message = checkAge(38);
+// console.log(message);
+
+/***** 2 ****/
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+//  if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   }
+//   return "Access denied, wrong password!";
+//   // Change code above this line
+// }
+// let message = checkPassword()
+// message = checkPassword("mangohackzor");
+// console.log(message);
+// message = checkPassword("polyhax");
+// console.log(message);
+// message = checkPassword("jqueryismyjam");
+// console.log(message);
+
+/***** 3 ****/
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+//   if (ordered > available) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+//   return 'The order is accepted, our manager will contact you';
+//   // Change code above this line
+// }
+// console.log(checkStorage(100, 50))
+// console.log(checkStorage(100, 130))
+// console.log(checkStorage(70, 0))
+// console.log(checkStorage(200, 20))
+// console.log(checkStorage(200, 250))
+// console.log(checkStorage(150, 0));
+
+/***** 4 ****/
+// // Change code below this line
+// const fruits = ["apple", "plum", "pear", "orange"]
+// console.log(fruits);
+
+/***** 5 ****/
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// // Change code below this line
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[fruits.length -1];
+
+// console.log(firstElement);
+// console.log(secondElement);
+// console.log(lastElement);
+
+/***** 6 ****/
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// // Write your code under this line
+// fruits[1] = "peach";
+// fruits[3] = "banana"
+// console.log(fruits);
+
+/***** 7 ****/
+// const fruits = ["apple", "peach", "pear", "banana"];
+// const fruitsArrayLength = fruits.length;
+// // Change code below this line
+// console.log(fruitsArrayLength);
+
+/***** 8 ****/
+// const fruits = ["apple", "peach", "pear", "banana"];
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+// // Change code below this line
+// console.log(lastElementIndex);
+// console.log(lastElement);
